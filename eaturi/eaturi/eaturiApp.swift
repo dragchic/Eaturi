3956ce714ef8b3742654b28087648b81a902ae8f
Raphael <raphaelghakim@gmail.com>
2025-03-20 10:46:35 +0700

    Initial Commit

---
+++ eaturi/eaturi/eaturiApp.swift
@@ -0,0 +1,17 @@
+//
+//  eaturiApp.swift
+//  eaturi
+//
+//  Created by Raphael Gregorius on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct eaturiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
