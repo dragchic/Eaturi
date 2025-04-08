29d647d86416f47b26a24f280eea699a9d683f5d
Raphael <raphaelghakim@gmail.com>
2025-04-08 07:47:22 +0700

    Rapihin unused struct and seperate customTabBarItem with miantabview

--- eaturi/eaturi/MainTabView.swift
+++ eaturi/eaturi/MainTabView.swift
@@ -51,32 +51,6 @@ struct MainTabView: View {
     }
 }
 
-
-struct CustomTabBarItem: View {
-    let icon: String
-    let title: String
-    let isSelected: Bool
-    let color: Color
-    
-    var body: some View {
-        VStack(spacing: 5) {
-            Rectangle()
-                .fill(isSelected ? color : Color.clear)
-                .frame(height: 5)
-                .cornerRadius(16)
-            Spacer()
-            Image(systemName: icon)
-                .font(.system(size: 25))
-                .foregroundColor(isSelected ? color : .gray)
-            Text(title)
-                .font(.caption)
-                .foregroundColor(isSelected ? color : .gray)
-            Spacer()
-        }
-        .frame(maxWidth: .infinity)
-    }
-}
-
 #Preview {
     do {
         let previewer = try Previewer()
